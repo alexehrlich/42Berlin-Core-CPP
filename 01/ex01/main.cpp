2e22bcb2bcb96ff0d3bba4f2943760dac5541a8b
Alexander Ehrlich <aehrlich@c4a7c8.42berlin.de>
2023-11-22 15:02:17 +0100

    add test

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aehrlich <aehrlich@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 08:11:07 by aehrlich          #+#    #+#             */
-/*   Updated: 2023/09/23 08:46:30 by aehrlich         ###   ########.fr       */
+/*   Updated: 2023/11/22 15:01:25 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 int	main(void)
 {
 	Zombie	*zombieHorde = Zombie::zombieHorde(3, "James");
+	
+	zombieHorde[1].setName("Alex");
+	zombieHorde[2].setName("Sophie");
 	delete [] zombieHorde;
 	return (0);
 }
