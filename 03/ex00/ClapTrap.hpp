77850f0bfe1b8c5d30a0d51950a7b0bfc1b1ae67
Alexander Ehrlich <alexehrlich@gmx.de>
2023-11-28 18:54:57 +0100

    ex00 temp

---
+++ 03/ex00/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aehrlich <aehrlich@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 11:49:17 by aehrlich          #+#    #+#             */
+/*   Updated: 2023/11/28 18:23:34 by aehrlich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+	public:
+		ClapTrap();
+		ClapTrap(const std::string& name);
+		~ClapTrap();
+		ClapTrap&	operator=(const ClapTrap& c);
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+	private:
+		std::string		_name;
+		unsigned int	_hitPoints;
+		int				_energyPoints;
+		int				_attackDamage;
+};
+
+#endif
--- 03/ex00/main.cpp
+++ 03/ex00/main.cpp
@@ -3,17 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aehrlich <aehrlich@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:54:10 by aehrlich          #+#    #+#             */
-/*   Updated: 2023/11/28 11:47:04 by aehrlich         ###   ########.fr       */
+/*   Updated: 2023/11/28 18:48:08 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "ClapTrap.hpp"
 
 int main( void )
 {
+	ClapTrap	cp1("John");
+	ClapTrap	cp2("Diana");
 
+	cp1.attack("Diana");
 	return 0;
 }
