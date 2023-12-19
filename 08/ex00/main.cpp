0ac9ac65fa33620ee1dca9904403ed661bc06411
Alexander Ehrlich <alexehrlich@gmx.de>
2023-12-19 16:41:49 +0100

    finish ex00

---
+++ 08/ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aehrlich <aehrlich@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 16:32:03 by aehrlich          #+#    #+#             */
+/*   Updated: 2023/12/19 16:39:12 by aehrlich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <vector>
+#include <iostream>
+
+int main()
+{
+	std::vector<int> numbers;
+
+	for (int i = 0; i < 5; i++)
+		numbers.push_back(i);
+
+	try
+	{
+		int idx = easyfind(numbers, 4);
+		std::cout << "Found target on index: " << idx << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
