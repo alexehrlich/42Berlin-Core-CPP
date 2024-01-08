0f21cfa197947f22bf624ec191de5a69a87980e1
Alexander Ehrlich <aehrlich@c4c4c1.42berlin.de>
2024-01-08 17:09:16 +0100

    cosmetics

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aehrlich <aehrlich@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:38:28 by aehrlich          #+#    #+#             */
-/*   Updated: 2023/12/18 09:55:45 by aehrlich         ###   ########.fr       */
+/*   Updated: 2024/01/08 17:08:40 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ int main( void )
 {
 	int a = 2;
 	int b = 3;
-	::swap( a, b );
+	myfuncs::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min( a, b ) = " << myfuncs::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << myfuncs::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	::swap(c, d);
+	myfuncs::swap( c, d );
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min( c, d ) = " << myfuncs::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << myfuncs::max( c, d ) << std::endl;
 	return 0; 
 }
--- 07/ex00/whatever.hpp
+++ 07/ex00/whatever.hpp
@@ -1,36 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   FuncTemplates.hpp                                  :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aehrlich <aehrlich@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:27:12 by aehrlich          #+#    #+#             */
-/*   Updated: 2023/12/18 09:49:49 by aehrlich         ###   ########.fr       */
+/*   Updated: 2024/01/08 17:07:02 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FUNCTEMPLATES_HPP
 # define FUNCTEMPLATES_HPP
 
-template <typename T>
-void	swap(T& lhs, T& rhs)
-{
-	T	temp = rhs;
-	rhs = lhs;
-	lhs = temp;
-}
+namespace myfuncs
+{	
+	template <typename T>
+	void	swap(T& lhs, T& rhs)
+	{
+		T	temp = rhs;
+		rhs = lhs;
+		lhs = temp;
+	}
 
-template <typename T>
-T		min(const T& lhs, const T& rhs)
-{
-	return (lhs < rhs ? lhs : rhs);
-}
+	template <typename T>
+	T		min(const T& lhs, const T& rhs)
+	{
+		return (lhs < rhs ? lhs : rhs);
+	}
 
-template <typename T>
-T		max(const T& lhs, const T& rhs)
-{
-	return (lhs > rhs ? lhs : rhs);
+	template <typename T>
+	T		max(const T& lhs, const T& rhs)
+	{
+		return (lhs > rhs ? lhs : rhs);
+	}
 }
 
 
