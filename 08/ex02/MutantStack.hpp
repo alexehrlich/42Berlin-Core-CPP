8946767c0eea6c15f490a4817636dd524a6327a7
Alexander Ehrlich <aehrlich@c4a7c9.42berlin.de>
2024-01-11 18:05:32 +0100

    implement top to bottom order

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 16:39:43 by aehrlich          #+#    #+#             */
-/*   Updated: 2024/01/09 18:10:50 by aehrlich         ###   ########.fr       */
+/*   Updated: 2024/01/11 18:01:51 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,35 +33,35 @@ class MutantStack : public std::stack<T>
 		//Typedefs are used to make the code more readable but then als
 		//typename is required in order to use the typedef as a type
 		//So the typename keyword is used to tell the compiler that std::stack<T>::container_type::iterator refers to a type
-		typedef typename std::stack<T>::container_type::iterator iterator;
-		MutantStack<T>::iterator begin (void) {
-			return (this->c.begin());
-		}
-		MutantStack<T>::iterator end (void){
-			return (this->c.end());
-		}
-
-		typedef typename std::stack<T>::container_type::const_iterator	const_iterator;
-		MutantStack<T>::const_iterator begin (void) const {
-			return (this->c.begin());
-		}
-		MutantStack<T>::const_iterator end (void) const {
-			return (this->c.end());
-		}
-
-		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::iterator reverse_iterator;
 		MutantStack<T>::reverse_iterator rbegin (void) {
+			return (this->c.begin());
+		}
+		MutantStack<T>::reverse_iterator rend (void){
+			return (this->c.end());
+		}
+
+		typedef typename std::stack<T>::container_type::const_iterator	const_reverse_iterator;
+		MutantStack<T>::const_reverse_iterator rbegin (void) const {
+			return (this->c.begin());
+		}
+		MutantStack<T>::const_reverse_iterator rend (void) const {
+			return (this->c.end());
+		}
+
+		typedef typename std::stack<T>::container_type::reverse_iterator iterator;
+		MutantStack<T>::iterator begin (void) {
 			return (this->c.rbegin());
 		}
-		MutantStack<T>::reverse_iterator rend (void) {
+		MutantStack<T>::iterator end (void) {
 			return (this->c.rend());
 		}
 
-		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
-		MutantStack<T>::const_reverse_iterator rbegin (void) const {
+		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_iterator;
+		MutantStack<T>::const_iterator begin (void) const {
 			return (this->c.rbegin());
 		}
-		MutantStack<T>::const_reverse_iterator rend (void) const {
+		MutantStack<T>::const_iterator end (void) const {
 			return (this->c.rend());
 		}
 };
