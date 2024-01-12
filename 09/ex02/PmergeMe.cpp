fe36eb8a8b5a234fe150bc10a88314b09bd404b4
Alexander Ehrlich <aehrlich@c4a7c10.42berlin.de>
2024-01-12 09:56:35 +0100

    error in time calc

--- 09/ex02/PmergeMe.cpp
+++ 09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 15:33:30 by aehrlich          #+#    #+#             */
-/*   Updated: 2024/01/08 16:06:54 by aehrlich         ###   ########.fr       */
+/*   Updated: 2024/01/12 09:54:36 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ void	PmergeMe::printResults()
 	std::cout << "Before:\t"; _printVector(_unsortedVec);
 	std::cout << "After:\t"; _printVector(_sortedVec);
 	std::cout << "After:\t"; _printDeque(_sortedDeque);
-	double vectorTime = static_cast<double>(_endTimeVec - _startTimeVec) / CLOCKS_PER_SEC * 1000000.0;
-	double dequeTime = static_cast<double>(_endTimeDeque - _startTimeDeque) / CLOCKS_PER_SEC * 1000000.0;
+	double vectorTime = static_cast<double>(_endTimeVec - _startTimeVec) / (CLOCKS_PER_SEC * 1000000);
+	double dequeTime = static_cast<double>(_endTimeDeque - _startTimeDeque) / (CLOCKS_PER_SEC * 1000000);
 	
 	std::cout << "Time to process a range of " << _unsortedVec.size() << " elements with std::vector<int>:\t" << vectorTime << " us" << std::endl;
 	std::cout << "Time to process a range of " << _unsortedVec.size() << " elements with std::deque<int> :\t" << dequeTime << " us" << std::endl;
